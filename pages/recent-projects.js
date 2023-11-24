92d8f86ff3fc4cca0b1871304c255ee0eed1cc18
AWesleyK <wesley.kellett@yahoo.com>
2023-11-24 15:52:08 -0600

    Adding RealWork Widget

--- pages/recent-projects.js
+++ pages/recent-projects.js
@@ -1,6 +1,7 @@
 // pages/recent-projects.js
 import React, { useEffect } from 'react';
 import RecentProjects from '@/components/RecetProjects/RecentProjects';
+import Layout from '@/components/Layout/Layout';
 
 const RecentProjectsPage = () => {
     return (
