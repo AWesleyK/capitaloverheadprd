ed29fa09c15ae4502ad1dc28bf58fe50c4c81dcf
AWesleyK <wesley.kellett@yahoo.com>
2023-11-24 15:48:24 -0600

    Adding RealWork Widget

---
+++ components/RecetProjects/RecentProjects.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const RecentProjects = () => {
+
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://app.realworklabs.com/static/plugin/loader.js?v=' + new Date().getTime();
+        script.async = true;
+    
+        script.onload = () => {
+          window.addEventListener('rwlPluginReady', () => {
+            window.rwlPlugin.init('https://app.realworklabs.com', 'EgasJIOpJO7pyX45');
+          }, false);
+        };
+    
+        document.body.appendChild(script);
+    
+        return () => {
+          document.body.removeChild(script);
+        };
+      }, []);
+    
+      return (
+        <div>
+          <h1>Recent Projects</h1>
+          <div id="rwl-output"></div>
+        </div>
+      );
+}
+
+export default RecentProjects;
--- pages/recent-projects.js
+++ pages/recent-projects.js
@@ -1,32 +1,13 @@
 // pages/recent-projects.js
 import React, { useEffect } from 'react';
+import RecentProjects from '@/components/RecetProjects/RecentProjects';
 
-const RecentProjects = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://app.realworklabs.com/static/plugin/loader.js?v=' + new Date().getTime();
-    script.async = true;
-
-    script.onload = () => {
-      window.addEventListener('rwlPluginReady', () => {
-        window.rwlPlugin.init('https://app.realworklabs.com', 'EgasJIOpJO7pyX45');
-      }, false);
-    };
-
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
-  return (
-    <div>
-      <h1>Recent Projects</h1>
-      <div id="rwl-output"></div>
-    </div>
-  );
+const RecentProjectsPage = () => {
+    return (
+        <Layout>
+          <RecentProjects />
+        </Layout>
+      );
 }
 
-export default RecentProjects;
+export default RecentProjectsPage;
